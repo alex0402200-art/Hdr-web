4bd026eedbfa9a75215cf8d6fa177509213967e7
alex0402200-art <alex0402200@gmail.com>
2026-09-12 02:55:03 +0700

    Reduce PBKDF2 iterations from 150000 to 100000

--- functions/_utils.js
+++ functions/_utils.js
@@ -22,7 +22,7 @@ export async function hashPassword(password, saltB64) {
   const salt = saltB64 ? fromB64(saltB64) : crypto.getRandomValues(new Uint8Array(16));
   const key = await crypto.subtle.importKey('raw', enc.encode(password), 'PBKDF2', false, ['deriveBits']);
   const bits = await crypto.subtle.deriveBits(
-    { name: 'PBKDF2', salt, iterations: 150000, hash: 'SHA-256' },
+    { name: 'PBKDF2', salt, iterations: 100000, hash: 'SHA-256' },
     key,
     256
   );
